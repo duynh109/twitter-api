5db9b052a60e1d473cfce485443339f906dfa6b6
duynh109 <duy10092002@gmail.com>
2024-04-08 21:52:07 +0700

    chore: add middleware error handler and message validation

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,9 +1,10 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import { ParamsDictionary } from 'express-serve-static-core'
 import User from '~/models/schemas/User.schema'
 import databseService from '~/services/database.services'
 import userService from '~/services/users.services'
 import { RegisterReqBody } from '~/models/requests/User.requests'
+import { nextTick } from 'process'
 
 export const loginController = (req: Request, res: Response) => {
   const { email, password } = req.body
@@ -17,17 +18,15 @@ export const loginController = (req: Request, res: Response) => {
   })
 }
 
-export const registerController = async (req: Request<ParamsDictionary, any, RegisterReqBody>, res: Response) => {
-  try {
-    const result = await userService.register(req.body)
-    return res.json({
-      message: 'register successful',
-      result
-    })
-  } catch (error) {
-    return res.status(400).json({
-      message: 'register failed',
-      error: error
-    })
-  }
+export const registerController = async (
+  req: Request<ParamsDictionary, any, RegisterReqBody>,
+  res: Response,
+  next: NextFunction
+) => {
+  throw new Error('test error')
+  const result = await userService.register(req.body)
+  return res.json({
+    message: 'register successful',
+    result
+  })
 }
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import express from 'express';
-import  databaseService  from './services/database.services';
-import usersRouter from './routes/users.routes';
+import express from 'express'
+import databaseService from './services/database.services'
+import usersRouter from './routes/users.routes'
+import { defaultErrorHandler } from './middlewares/error.middlewares'
+
+databaseService.connect()
 const app = express()
 const port = 3000
-
 app.use(express.json())
 app.use('/users', usersRouter)
-databaseService.connect();
-
+app.use(defaultErrorHandler)
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
