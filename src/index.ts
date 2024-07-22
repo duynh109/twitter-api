299d13ad980ebaff30c228f664e4cac0555c37a2
duynh109 <duy10092002@gmail.com>
2024-07-22 11:11:57 +0700

    feature(conversations): conversation schema, route get conversation, standardize sent conversation

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,10 @@ import likesRouter from './routes/likes.routes'
 import searchRouter from './routes/search.routes'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
+import Conversation from './models/schemas/Conversation.schema'
+import conversationsRouter from './routes/conversations.routes'
+import { ObjectId } from 'mongodb'
+import { result } from 'lodash'
 // import '~/utils/fake ' // fake dữ liệu
 
 config()
@@ -39,6 +43,7 @@ app.use('/tweets', tweetsRouter)
 app.use('/bookmarks', bookmarksRouter)
 app.use('/likes', likesRouter)
 app.use('/search', searchRouter)
+app.use('/conversations', conversationsRouter)
 app.use('/static', staticRouter)
 app.use('/static/video', express.static(UPLOAD_VIDEO_DIR))
 
@@ -57,16 +62,29 @@ const users: {
 io.on('connection', (socket) => {
   console.log(`user ${socket.id} connected`)
   const user_id = socket.handshake.auth._id
+
   users[user_id] = {
     socket_id: socket.id
   }
   console.log(users)
-  socket.on('private message', (data) => {
-    const receiver_socket_id = users[data.to]?.socket_id
+
+  socket.on('send_message', async (data) => {
+    const { receiver_id, sender_id, content } = data.payload
+    const receiver_socket_id = users[receiver_id]?.socket_id
     if (!receiver_socket_id) return
-    socket.to(receiver_socket_id).emit('receive private message', {
-      content: data.content,
-      from: user_id
+
+    const conversation = new Conversation({
+      sender_id: new ObjectId(sender_id),
+      receiver_id: new ObjectId(receiver_id),
+      content: content
+    })
+
+    const result = await databaseService.conversations.insertOne(conversation)
+
+    conversation._id = result.insertedId
+
+    socket.to(receiver_socket_id).emit('receive_message', {
+      payload: conversation
     })
   })
   socket.on('disconnect', () => {
